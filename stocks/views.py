ef213ea18e7d373541aa3d5344e4e6b08c03b68d
shamsurlm10 <shamsur.works@gmail.com>
2023-12-10 15:40:30 +0600

    added line chart

--- stocks/views.py
+++ stocks/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Stock
-from .forms import StockForm  # Create a form for adding/editing stock data
+from .forms import StockForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Stock
-def home(request):
-    # Get all stocks
-    stocks = Stock.objects.all()
 
-    # Configure pagination
+def home(request):
+    stocks = Stock.objects.all()
+    
     paginator = Paginator(stocks, 10)  # Show 10 stocks per page
     page = request.GET.get('page')
 
     try:
         stocks = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver the first page
         stocks = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g., 9999), deliver the last page
         stocks = paginator.page(paginator.num_pages)
 
     return render(request, 'stocks/home.html', {'stocks': stocks})  
@@ -36,22 +33,6 @@ def stock_create(request):
     else:
         form = StockForm()
     return render(request, 'stocks/stock_form.html', {'form': form})
-
-# def stock_edit(request, pk):
-#     stock = get_object_or_404(Stock, pk=pk)
-#     if request.method == 'POST':
-#         form = StockForm(request.POST, instance=stock)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = StockForm(instance=stock)
-#     return render(request, 'stocks/stock_form.html', {'form': form})
-
-# def stock_delete(request, pk):
-#     stock = get_object_or_404(Stock, pk=pk)
-#     stock.delete()
-#     return redirect('home')
 
 def stock_edit(request, pk):
     stock = get_object_or_404(Stock, pk=pk)
